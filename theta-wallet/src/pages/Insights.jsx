334aea4b2b3368a78bd8b2aaa6e178b00f34877f
Godfred <goddysmart2468@gmail.com>
2024-07-11 13:02:29 +0000

    General Interface

---
+++ theta-wallet/src/pages/Insights.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Insights = () => {
+  return (
+    <div>
+      This is our Financial Insights Page
+    </div>
+  )
+}
+
+export default Insights
---
+++ theta-wallet/src/pages/index.jsx
@@ -0,0 +1,5 @@
+export { default as Dashboard } from './Dashboard';
+export { default as Expenses } from './Expenses';
+export { default as Insights } from './Insights';
+export { default as Predictions } from './Predictions';
+export { default as Payments } from './Payments';
