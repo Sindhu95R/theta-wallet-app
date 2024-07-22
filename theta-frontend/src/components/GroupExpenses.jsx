70a06c5aacd7f15a726bcf1db7c84c0bb5052d3d
Godfred <goddysmart2468@gmail.com>
2024-07-22 14:55:51 +0000

    update

--- theta-frontend/src/components/GroupExpenses.jsx
+++ theta-frontend/src/components/GroupExpenses.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import {
   Box, Typography, Button, Paper, Grid, Table,
   TableBody, TableCell, TableContainer, TableHead,
